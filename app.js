a6d8afd65a3684d0d7d187a3871c294e11a2b917
DAVIDCIOCOIU95 <david.samir.ciocoiu@gmail.com>
2020-05-16 16:14:12 +0200

    bootstrap added

--- app.js
+++ app.js
@@ -4,3 +4,17 @@ const ejs = require("ejs");
 const _ = require("lodash");
 const mongoose = require("mongoose");
 
+const app = express();
+app.set('view engine', 'ejs');
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(express.static("public"));
+mongoose.connect('mongodb://localhost:27017/Journal', {useNewUrlParser: true});
+
+app.get("/", (req, res) => {
+    res.render("body");
+});
+
+const port = 3000;
+app.listen(port, () => {
+    console.log("Server started successfully on port: " + port);
+});
